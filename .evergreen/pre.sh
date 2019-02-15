0978dd7669c7571006d58f1f4cd4960cccc0ed24
Kevin Albertson <kevin.albertson@mongodb.com>
2019-02-15 13:46:19 -0500

    dammit

--- .evergreen/pre.sh
+++ .evergreen/pre.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-set -o xtrace   # Write all commands first to stderr
-set -o errexit  # Exit the script with error if any of the commands fail
+set -o xtrace
 
 echo "Machine environment"
 git --version
