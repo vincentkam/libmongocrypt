fdeb54e0d5fb4ca2ccdf7d0698be0de993ce4ff1
Kevin Albertson <kevin.albertson@mongodb.com>
2019-02-16 11:35:24 -0500

    In progress

--- .evergreen/pre.sh
+++ .evergreen/pre.sh
@@ -4,5 +4,15 @@ set -o xtrace
 
 echo "Machine environment"
 git --version
-openssl --version
+openssl version
 python --version
+
+if which gcc; then
+    gcc --version
+fi
+
+if which clang; then
+    clang --version
+fi
+
+cmake --version
